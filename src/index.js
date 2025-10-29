e237ea9cc092985b292a91fca48de6b65e24ad17
Aayush <94561475+AayushMainali-Github@users.noreply.github.com>
2025-10-29 23:18:17 +0530

    Import/Export bug fix

--- src/index.js
+++ src/index.js
@@ -4,18 +4,18 @@
 // ===========================
 
 // Arithmetic Functions
-export { extendedGCD } from "./src/arithmetic/extendedGCD.js";
-export { gcd } from "./src/arithmetic/gcd.js";
-export { lcm } from "./src/arithmetic/lcm.js";
-export { mod } from "./src/arithmetic/mod.js";
+export { extendedGCD } from "./arithmetic/extendedGCD.js";
+export { gcd } from "./arithmetic/gcd.js";
+export { lcm } from "./arithmetic/lcm.js";
+export { mod } from "./arithmetic/mod.js";
 
 // Prime Functions
-export { isPrime } from "./src/prime/isPrime.js";
-export { sieve } from "./src/prime/sieve.js";
-export { sieveRange } from "./src/prime/sieveRange.js";
+export { isPrime } from "./prime/isPrime.js";
+export { sieve } from "./prime/sieve.js";
+export { sieveRange } from "./prime/sieveRange.js";
 
 // Sequence Functions
-export { fibonacci } from "./src/sequence/fibonacci.js";
-export { lucas } from "./src/sequence/lucas.js";
-export { nthFibonacci } from "./src/sequence/nthFibonacci.js";
-export { nthLucas } from "./src/sequence/nthLucas.js";
+export { fibonacci } from "./sequence/fibonacci.js";
+export { lucas } from "./sequence/lucas.js";
+export { nthFibonacci } from "./sequence/nthFibonacci.js";
+export { nthLucas } from "./sequence/nthLucas.js";
